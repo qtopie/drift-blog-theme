2bd652a0a77726cefa824000d8cf18327b933528
qtopie <qtopie.rw@gmail.com>
2026-01-25 21:06:58 +0800

    add mermaid support

--- react-app/src/pages/page.tsx
+++ react-app/src/pages/page.tsx
@@ -1,7 +1,6 @@
 // react-app/src/pages/single.tsx
 import ScrollToTop from '../components/ScrollToTop';
-import MermaidLoader from '../components/Mermaid';
 import { mountComponents } from '../utils/mount';
 
 // Mount a Fluent-styled composite component
-mountComponents('react-app-components', [ScrollToTop, MermaidLoader]);
+mountComponents('react-app-components', [ScrollToTop]);
